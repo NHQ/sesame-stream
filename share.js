141f06299eadbe054d82a9559a1fd1e98792eefe
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-03-17 20:19:11 -0700

    who care exclamation

--- share.js
+++ share.js
@@ -6,13 +6,19 @@ module.exports = function(stream, path, opts){
     if(e) {
       stream.pipe(e, {end: false})
       e.pipe(stream, {end: false})
+      e.on('end', function(){
+      })
+      stream.on('end', function(){
+      })
     }
   })
   var s = channels[path].connected.push(stream)
   stream.on('end', function(){
     channels[path].connected.splice(s, 1, null)
+    channels[path].connected = channels[path].connected.filter(Boolean)
+    if(!channels[path].connected.length) delete channels[path]
   })
   stream.on('error', function(err){
-    console.error(err)
+    //console.error(err) // who cares!
   })
 }
